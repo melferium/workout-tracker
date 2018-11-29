95ecf2b560c19fbc8287151af8d7025e1765bf7f
melferium <ferdinand.melchor@hotmail.com>
2018-11-29 18:28:45 +0800

    add apartment gems and scafold workouts

---
+++ db/migrate/20181129095737_reindex_users_by_email_and_subdomain.rb
@@ -0,0 +1,15 @@
+class ReindexUsersByEmailAndSubdomain < ActiveRecord::Migration[5.2]
+  def change
+
+    def up 
+      remove_index :users, :email
+      add_index :users, [:email, :subdomain], unique: true
+    end
+
+
+    def down
+      remove_index :users, [:email, :subdomain]
+      add_index :users, :email, unique: true
+    end
+  end
+end
